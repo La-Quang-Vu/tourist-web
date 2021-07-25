3ef248d0c589e5e4ac991485c6ac86bd125826ed
La-Quang-Vu <laquangvu2007@gmail.com>
2021-07-25 13:53:54 +0700

    Divided components HomeBanner, ReasonCombo, SearchBox

---
+++ src/pages/home/HomeBanner.js
@@ -0,0 +1,17 @@
+
+const HomeBanner = () => {
+  return (
+    <div id="homeBanner" className="invisible md:visible bg-homeBannerV2 bg-no-repeat bg-center bg-cover text-center h-[15px] md:h-auto md:pt-10 pb-[80px] md:pb-[210px]">
+      <div className="h-[82px]">
+        <p className=" text-3xl font-bold mb-[5px] text-white">
+          Find cheap hotel and flight deals
+        </p>
+        <p className="text-lg text-white">
+          A great journey starts with one click
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default HomeBanner;
