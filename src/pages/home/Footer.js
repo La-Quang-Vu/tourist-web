b771075bf12a026a460e114b22c58185ac38532f
La-Quang-Vu <laquangvu2007@gmail.com>
2021-07-31 15:09:44 +0700

    Add footer middle

--- src/pages/home/Footer.js
+++ src/pages/home/Footer.js
@@ -64,7 +64,77 @@ const Footer = () => {
             
           </ul>
         </div>
-        <div></div>
+        <div className="py-[15px] md:flex">
+          <div className="md:w-1/5">
+            <a href="" className="mb-[5px] block text-sm hover:underline">About Vntrip</a>
+            <a href="" className="mb-[5px] block text-sm hover:underline">Contact</a>
+            <a href="" className="mb-[5px] block text-sm hover:underline">Terms of Use</a>
+            <a href="" className="mb-[5px] block text-sm hover:underline">Operation Regulation</a>
+            <a href="" className="mb-[5px] block text-sm hover:underline">Privacy Policy</a>
+            <a href="" className="mb-[5px] block text-sm hover:underline">Tourism blog</a>
+            <a href="" className="mb-[5px] block text-sm hover:underline">Recruitment</a>
+            <a href="" className="mb-[5px] block text-sm hover:underline">Vntrip cashback</a>
+            <a href="" className="mb-[5px] block text-sm hover:underline">Affiliate Cooperation</a>
+          </div>
+          <div className="md:w-1/5 md:px-[15px] md:py-[5px]">
+            <a href="" className="md:mb-[10px] block">
+              <img src="../../assets/images/footer/appStore.png" alt="appStore"
+                className="w-[120px]"
+              />
+            </a>
+            <a href="" className="md:mb-[10px] block">
+              <img src="../../assets/images/footer/googlePlay.png" alt="appStore"
+                className="w-[120px]"
+              />
+            </a>
+          </div>
+          <div className="md:w-[30%] md:pr-[15px]">
+            <p className="mb-[5px] font-semibold text-sm">VNTRIP Co Ltd.,.</p>
+            <p className="mb-[5px] font-semibold text-sm">Floor 2, Building 17T4 Hapulico Complex</p>
+            <p className="mb-[5px] text-sm">1 Nguyen Huy Tuong Str., Thanh Xuan Dist., Hanoi</p>
+            <p className="mb-[5px] text-sm">Account number: 0491000136688</p>
+            <p className="mb-[5px] text-sm">Bank: Vietcombank</p>
+            <p className="mb-[5px] text-sm">Branch: Thăng Long</p>
+            <p className="mb-[5px] text-sm">Date of issue business permit: 9/5/2016</p>
+            <p className="mb-[5px] text-sm">
+              Email:&nbsp;
+              <a href="mailto:cs@vntrip.vn" className="font-semibold text-[#1890ff] hover:underline">cs@vntrip.vn</a>
+            </p>
+            <p className="mb-[5px] text-sm">
+              Hotline:&nbsp;
+              <a href="tel:096 326 6688" className="font-semibold text-[#389e0d] hover:underline">096 326 6688</a>
+            </p>
+          </div>
+          <div className="md:w-[30%]">
+            <p className="mb-[15px] text-sm">
+            Subscribe now to receive coupons, promotions &amp; secret deals:
+            </p>
+            <form className="mb-[15px] flex">
+              <div className="flex-1">
+                <div>
+                  <input type="text" placeholder="Your Email" name="email"
+                    className="w-full h-10 border rounded px-[15px] text-sm hover:border-[#48ABFD] focus:border-[#48ABFD] transition-all duration-300 outline-none focus:shadow-input1"
+                  ></input>
+                </div>
+              </div>
+              <button type="button" className="bg-[#ff8917] border border-[#ff8917] hover:bg-[#FF952F] hover:border-[#FF952F] transition-all duration-200 px-[15px] h-10 rounded">
+                <span className="text-sm text-white font-semibold">Register</span>
+              </button>
+            </form>
+            <div className="flex justify-center">
+              <a target="_blank" href="http://online.gov.vn/Home/WebDetails/65609">
+                <img alt="Logo bộ công thương" src="../../assets/images/footer/logo_bocongthuong_blue.png" 
+                  className="w-[150px]"
+                />
+              </a>
+              <a target="_blank" href="http://online.gov.vn/Home/WebDetails/24227">
+                <img alt="Logo bộ công thương" src="../../assets/images/footer/logo_bocongthuong_red.png" 
+                  className="w-[150px]"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
